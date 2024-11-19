2002632c1a99ec1ec36559272e46d220431295e3
Lock-el <fillod.martial@gmail.com>
2024-11-19 11:37:23 +0100

    Update player.py

--- player.py
+++ player.py
@@ -48,8 +48,8 @@ class Player(Entity):
             self.force_x = self.direction[1] / coef
         print(self.force_x)
           # * self.velocity[0]
-        self.force_y = self.direction[1] / coef - self.GRAVITY #ou comme ça
-        self.apply_force(pygame.math.Vector2(self.force_x,self.force_y)) #mieux comme ça
+        self.force_y = self.direction[1] / coef - self.GRAVITY
+        self.apply_force(pygame.math.Vector2(self.force_x,self.force_y))
         
 
 
